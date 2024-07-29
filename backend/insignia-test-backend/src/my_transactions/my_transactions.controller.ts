0bb7a1c45908cd35c33c9420c14a25c30c697d65
Andika Nugraha <andikanzz@gmail.com>
2024-07-29 14:17:48 +0700

    update swagger for my transactions

--- backend/insignia-test-backend/src/my_transactions/my_transactions.controller.ts
+++ backend/insignia-test-backend/src/my_transactions/my_transactions.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
 import { MyTransactionsService } from './my_transactions.service';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { MyTransactionsEntity } from './entities/my_transactions.entity';
 
 @Controller('my_transactions')
+@ApiTags('my_transactions')
 export class MyTransactionsController {
   constructor(private myTransactionsService: MyTransactionsService) {}
 
